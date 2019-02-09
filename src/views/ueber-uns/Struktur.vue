878d5700a9fc1fc27da7c87e7e21c11b72cb1115
DerZade <DerZade@gmail.com>
2019-02-09 15:31:44 +0100

    Updated vue to 2.6.x

--- src/views/ueber-uns/Struktur.vue
+++ src/views/ueber-uns/Struktur.vue
@@ -1,14 +1,14 @@
 <template>
     <Content>
-        <template slot="left">
+        <template v-slot:left>
             <p>Leftiii</p>
         </template>
         <template>
             <Container> 
-                <template slot="header">
+                <template v-slot:header>
                     <span>Aufbau</span>
                 </template>
-                <template slot="image">
+                <template v-slot:image>
                     <img class="grad-header__logo" src="@/assets/image.jpg">
                 </template>
                 <template>
@@ -16,46 +16,46 @@
                 </template>
             </Container>
             <Container headerColor="#66AA66"> 
-                <template slot="header">
+                <template v-slot:header>
                     <span>Gastspieler</span>
                 </template>
                 <template>
                     Du darfst an den meisten unserer Spiele teilnehmen. Du hast keine Pflichten in der Gruppe, aber auch kein Stimmrecht.
                 </template>
-                <template slot="footer">
+                <template v-slot:footer>
                     Im Forum vorstellen, TS-Gespräch führen, Nickname festlegen
                 </template>
             </Container>
             <Container headerColor="#66AA66"> 
-                <template slot="header">
+                <template v-slot:header>
                     <span>Stammspieler</span>
                 </template>
                 <template>
                     Wir ernennen Dich zum Stammspieler. Bei Inaktivität kann Dir der Stammspieler-Status wieder entzogen werden.
                 </template>
-                <template slot="footer">
+                <template v-slot:footer>
                     Regelmäßig dabei sein.
                 </template>
             </Container>
             <Container headerColor="#6CAACC"> 
-                <template slot="header">
+                <template v-slot:header>
                     <span>Anwärter</span>
                 </template>
                 <template>
                     Wenn du mehr willst, bewirb dich um die Mitgliedschaft. Nutze die Anwärterzeit, um die Adler von dir zu überzeugen.
                 </template>
-                <template slot="footer">
+                <template v-slot:footer>
                     Als Anwärter melden, Im Forum bewerben, Adler überzeugen
                 </template>
             </Container>
             <Container headerColor="#D18D1F"> 
-                <template slot="header">
+                <template v-slot:header>
                     <span>Adler</span>
                 </template>
                 <template>
                     Wir ernennen Dich zum Stammspieler. Bei Inaktivität kann Dir der Stammspieler-Status wieder entzogen werden.
                 </template>
-                <template slot="footer">
+                <template v-slot:footer>
                     Zugang zu Interna in Discord, Forum, Wiki, Stimmrecht
                 </template>
             </Container>
