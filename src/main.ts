1f8c011cd4ba4fcdb4716fc16e55080daefbe406
DerZade <DerZade@gmail.com>
2019-02-11 16:42:08 +0100

    Added hint for IE users (Fixes #4)

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,35 @@ import './globalComponents';
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app');
+/* tslint:disable */
+function detectIE() {
+  var ua = window.navigator.userAgent;
+
+  var msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+    // IE 10 or older
+    return true;;
+  }
+
+  var trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+    // IE 11
+
+    return true;
+  }
+
+  // other browser
+  return false;
+}
+/* tslint:enable */
+
+if (!detectIE()) {
+  const ieHint = document.getElementById('IEHint');
+  if (ieHint) ieHint.remove();
+
+  new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app');
+}
+
