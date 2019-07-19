e0084d783af5c626dfc6b0faa82d1de6ef03e463
DerZade <DerZade@gmail.com>
2019-07-19 14:11:07 +0200

    First impl. of gallery items

---
+++ src/components/Gallery/Item.vue
@@ -0,0 +1,88 @@
+<template>
+<div :class="['gallery-item', `gallery-item--width-${size}`]">
+    <div class="gallery-item__wrapper">
+        <img class="gallery-item__image" src="" />
+        <div class="gallery-item__title-bar">
+            <span class="gallery-item__title">{{title}}</span>
+            <span class="gallery-item__author">{{author}}</span>
+        </div>
+    </div>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class GalleryItemVue extends Vue {
+    @Prop({ default: 1 }) private size!: number;
+
+    private author = 'nomisum';
+    private title = 'Touchdown at Extract';
+}
+</script>
+
+<style lang="scss" scoped>
+.gallery-item {
+    float: left;
+    margin-bottom: 35px;
+
+    margin-left: 35px;
+    
+    &#{&}--width-1 {
+        // reset 
+        width: calc(100% - 35px);
+    }
+
+    &#{&}--width-2 {
+        width: calc((100% - 70px) / 2);
+    }
+
+    &#{&}--width-3 {
+        width: calc((100% - 105px) / 3);
+    }
+
+    &__wrapper {
+        box-shadow: 0px 4px 60px rgba(0, 0, 0, 0.25);
+        background-color: #C4C4C4;
+        position: relative;
+        width: 100%;
+        padding-top: 56.25%;
+    }
+
+    &__title-bar,
+    &__image {
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        right: 0px;
+        bottom: 0px;
+    }
+
+    &__title-bar {
+        padding: 30px;
+        display: flex;
+        align-items: flex-end;
+        top: auto;
+        height: 100px;
+        max-height: 50%;
+        box-sizing: border-box;
+        background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.27) 40.62%, rgba(0, 0, 0, 0.75) 100%);
+    }
+
+    &__author,
+    &__title {
+        color: white;
+    }
+
+    &__author {
+        margin-left: 10px;
+        color: #C0C0C0;
+    }
+
+    // &#{&}--width-2 #{&}__title-bar > *,
+    // &#{&}--width-3 #{&}__title-bar > * {
+    //     display: none
+    // }
+}
+</style>
--- src/views/ueber-uns/Eindruecke.vue
+++ src/views/ueber-uns/Eindruecke.vue
@@ -1,13 +1,35 @@
 <template>
-<div>
-    <h1>Eindrücke</h1>
-</div>
+<Content>
+    <div class="gallery-wrapper">
+        <gallery-item v-for="(i, index) in items" :key="index" :size="i" />
+    </div>
+</Content>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import GalleryItemVue from '@/components/Gallery/Item.vue';
 
-@Component
-export default class UeberUnsEindruecke extends Vue {}
+@Component({
+    components: {
+        GalleryItem: GalleryItemVue
+    }
+})
+export default class UeberUnsEindruecke extends Vue {
+    private items: number[] = [];
+
+    private mounted() {
+        for (let i = 0; i < 40; i++) {
+            this.items.push(Math.floor(Math.random() * 3 + 1));
+        }
+    }
+}
 </script>
 
+<style lang="scss" scoped>
+.gallery-wrapper {
+    // we need extra 35px on the left side to compensate the 35px left margin of all tiles
+    width: calc(100% + 35px);
+    margin-left: -35px;
+}
+</style>
