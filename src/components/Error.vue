deb0cab77e297f5e7995b0af1e3e008d7b85e36b
simon.schmidt <simon.schmidt@idealo.de>
2019-07-22 01:18:34 +0200

    error images to svg

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,7 +1,7 @@
 <template functional>
     <div class="grad-error">
-        <img class="grad-error__image" v-if="props.type === '404'" src="@/assets/error/404.png" alt="error-image" />
-        <img class="grad-error__image" v-else src="@/assets/error/generic.png" alt="error-image" />
+        <img class="grad-error__image" v-if="props.type === '404'" src="@/assets/error/404.svg" alt="error-image" />
+        <img class="grad-error__image" v-else src="@/assets/error/generic.svg" alt="error-image" />
         <div class="grad-error__text">
             <slot v-if="$slots.default"></slot>
             <template v-else>Scheint so als ob etwas schief gelaufen ist.</template>
@@ -15,7 +15,7 @@
     align-items: center;
     flex-direction: column;
     &__image {
-        max-height: 220px;
+        max-height: 320px;
     }
 
     &__text {
