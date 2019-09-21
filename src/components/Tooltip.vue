67001413694e14a8b29015094a0b5e576deed465
DerZade <DerZade@gmail.com>
2019-09-21 22:49:42 +0200

    Added tooltip component

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="grad-tooltip" @mouseenter="shown = true;" @mouseleave="shown = false;">
+        <slot/>
+        <span ref="tooltip" v-if="shown" class="grad-tooltip__text">{{text}}</span>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
+
+@Component
+export default class TooltipVue extends Vue {
+    @Prop({ default: '' }) private text!: string;
+    private shown = false;
+
+    /**
+     * @description Adjusts position of tooltip to make sure it doesn't clip over the left side of the screen
+     * @author DerZade
+     */
+    @Watch('shown')
+    private fixClipping() {
+        if (!this.shown) return;
+
+        // try again on the next tick if the text field wrapper isn't rendered atm
+        if (!this.$refs.tooltip) return this.$nextTick(this.fixClipping);
+
+        // find offset from left screen border
+        const div = this.$refs.tooltip as HTMLSpanElement;
+        const left = div.getBoundingClientRect().left;
+
+        // make sure the wrapper is a minimum 8px away of the left screen border
+        if (left > 8) {
+            div.style.marginLeft = null;
+        } else {
+            const prevMargin = div.style.marginLeft ? Number.parseFloat(div.style.marginLeft) : 0;
+
+            div.style.marginLeft = `${prevMargin + 8 - left}px`;
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.grad-tooltip {
+    position: relative;
+    display: flex;
+    justify-content: center;
+
+
+    &__text {
+        color: white;
+        white-space: nowrap;
+        background-color: rgba(black, 0.8);
+        padding: 8px;
+        border-radius: 4px;
+        position: absolute;
+        font-size: 14px;
+        font-weight: bold;
+        top: 0px;
+        letter-spacing: 0.08em;
+        transform: translateY( calc(-100% - 4px) );
+    }
+}
+</style>
--- src/plugins/globalComponents.ts
+++ src/plugins/globalComponents.ts
@@ -4,8 +4,10 @@ import ContentVue from '@/components/Content.vue';
 import ContainerVue from '@/components/Container.vue';
 import ErrorVue from '@/components/Error.vue';
 import LoaderVue from '@/components/Loader.vue';
+import TooltipVue from '@/components/Tooltip.vue';
 
 Vue.component('Content', ContentVue);
 Vue.component('Container', ContainerVue);
 Vue.component('Error', ErrorVue);
 Vue.component('Loader', LoaderVue);
+Vue.component('Tooltip', TooltipVue);
