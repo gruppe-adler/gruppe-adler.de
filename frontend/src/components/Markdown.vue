ad1cbbf515f9f5b34e717f379f065ed883c26537
DerZade <DerZade@gmail.com>
2020-06-25 14:58:42 +0200

    Open links in markdown always in new tab

--- frontend/src/components/Markdown.vue
+++ frontend/src/components/Markdown.vue
@@ -6,12 +6,27 @@
 import { Component, Vue, Prop } from 'vue-property-decorator';
 import MarkdownIt from 'markdown-it';
 
+const md = MarkdownIt({ html: true });
+
+// Remember old renderer, if overridden, or proxy to default renderer
+const defaultRender = md.renderer.rules.link_open || function (tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+};
+
+// eslint-disable-next-line @typescript-eslint/camelcase
+md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+    tokens[idx].attrPush(['target', '_blank']); // add new attribute
+
+    // pass token to default renderer.
+    return defaultRender(tokens, idx, options, env, self);
+};
+
 @Component
 export default class MarkdownVue extends Vue {
     @Prop({ required: true, type: String }) private md!: string;
 
     private get renderedHTML (): string {
-        return MarkdownIt({ html: true }).render(this.md, {});
+        return md.render(this.md, {});
     }
 }
 </script>
