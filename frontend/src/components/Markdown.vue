5004ffa4e5290c86ff461069de7ab3e250301a3c
DerZade <DerZade@gmail.com>
2020-11-15 23:30:55 +0100

    Switch markdown to marked

--- frontend/src/components/Markdown.vue
+++ frontend/src/components/Markdown.vue
@@ -4,20 +4,29 @@
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator';
-import MarkdownIt from 'markdown-it';
+import marked from 'marked';
 
-const md = MarkdownIt({ html: true });
+const renderer = new marked.Renderer();
 
-// Remember old renderer, if overridden, or proxy to default renderer
-const defaultRender = md.renderer.rules.link_open || function (tokens, idx, options, env, self) {
-    return self.renderToken(tokens, idx, options);
-};
+renderer.link = function (href, title, text) {
+    let external = true;
 
-md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
-    tokens[idx].attrPush(['target', '_blank']); // add new attribute
+    if (href !== null) {
+        try {
+            const url = new URL(href);
+            external = (url.host !== window.location.host);
+        } catch (err) {
+            external = false;
+        }
+    }
 
-    // pass token to default renderer.
-    return defaultRender(tokens, idx, options, env, self);
+    const titleStr = title === null ? '' : ` title="${title}"`;
+
+    if (external) {
+        return `<a target="_blank" href="${href}"${titleStr}>${text}</a>`;
+    } else {
+        return `<a href="${href}"${titleStr}>${text}</a>`;
+    }
 };
 
 @Component
@@ -25,7 +34,7 @@ export default class MarkdownVue extends Vue {
     @Prop({ required: true, type: String }) private md!: string;
 
     private get renderedHTML (): string {
-        return md.render(this.md, {});
+        return marked(this.md, { renderer });
     }
 }
 </script>
