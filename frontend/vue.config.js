93cfe8aa30c2e9a71bb69aec32984aa47b94cce4
DerZade <DerZade@gmail.com>
2020-12-04 13:03:05 +0100

    Improve preloading

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,5 +2,14 @@ module.exports = {
     pwa: {
         themeColor: '#000000',
         name: 'Gruppe Adler'
+    },
+    chainWebpack: config => {
+        config.plugin('preload').tap(options => {
+            options[0].include = 'all';
+
+            options[0].fileBlacklist = options[0].fileBlacklist || [];
+            options[0].fileBlacklist.push(/\/admin/)
+            return options
+        });
     }
 }
