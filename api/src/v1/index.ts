6b88c6dcd524402ffa7fdc1078129d173a36221f
DerZade <DerZade@gmail.com>
2023-08-02 09:33:26 +0200

    remove everything twitter related

--- api/src/v1/index.ts
+++ api/src/v1/index.ts
@@ -3,7 +3,6 @@ import { globalErrorHandler } from '../utils/express';
 import pageRouter from './routes/page.router';
 import containerRouter from './routes/container.router';
 import uploadRouter from './routes/upload.router';
-import twitterRouter from './routes/twitter.router';
 import eventsRouter from './routes/events.router';
 
 const v1Router = Router();
@@ -11,7 +10,6 @@ const v1Router = Router();
 v1Router.use('/page', pageRouter);
 v1Router.use('/container', containerRouter);
 v1Router.use('/upload', uploadRouter);
-v1Router.use('/twitter', twitterRouter);
 v1Router.use('/events', eventsRouter);
 
 v1Router.use(globalErrorHandler);
