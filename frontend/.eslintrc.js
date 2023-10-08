867903c6b4c2d9b5295a7c4f9509336b46e8fac0
DerZade <DerZade@gmail.com>
2023-10-08 13:19:47 +0200

    chore(frontend): :recycle: update vuecli project template

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -14,6 +14,8 @@ module.exports = {
     rules: {
         indent: ['error', 4],
         semi: ['error', 'always'],
+        'vue/multi-word-component-names': 'off',
+        'vue/no-reserved-component-names': 'off',
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
     }
